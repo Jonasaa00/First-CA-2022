63c73822a4b5e598d5f3fe6e21b88da419d76b66
Jonasaa00 <jonasaarhaug@gmail.com>
2022-01-23 18:42:14 +0100

    done¨

---
+++ index.js
@@ -0,0 +1,96 @@
+// Question 1
+ /*
+var outOfStock = true;
+ if (outOfStock == true) {
+    console.log ("Out of stock")
+ }
+
+ else {
+    console.log ("In stock")}
+*/
+
+
+// Question 2
+/*
+for (var x = 15; x <= 25; x++) {
+if (x == 17 || x == 20) {
+    console.log(x);
+   }
+}
+*/
+
+
+// Question 3
+/*
+var games = [
+    {
+     title: "Grand Theft Auto",
+     rating: 4.48,
+    },
+    {
+     title: "Portal 2",
+     rating: 3.5,
+    },
+    {
+     title: "Team Fortress",
+     rating: null,
+    },
+    {
+     title: "The Witcher",
+     rating: 3.0,
+    },
+    {
+     title: "The Elder Scrolls",
+     rating: 2.9,
+    },
+   ];
+
+   for (var i = 0; i < games.length; i++) {
+       if (games[i].rating <= 3.5) {
+           console.log (games[i].title);
+           console.log (games[i].rating);
+       }
+   }
+*/
+
+
+// Question 4
+/* 
+    function whatIDontLike (things) {
+    if (typeof things === "string"){
+        console.log("I dont like" + things);
+    }
+    else {
+        console.log("please send in a string");
+    }
+}
+
+whatIDontLike("winter");
+whatIDontLike(29);
+whatIDontLike(null);
+whatIDontLike(true);
+*/
+
+
+// Question 5
+
+
+
+
+
+
+
+
+
+// Question 6
+
+var arr = [0, 1, 4, 9, 16];
+function find(arr, value) {
+    
+}
+
+
+
+
+
+// Question 7
